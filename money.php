a08dc8573c07d49050741cdd6041a96d1ee22c5f
MaxZhukov86 <maxjukov@0gmail.com>
2015-12-19 10:33:01 +0200

    third

---
+++ money.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+class CMoney{
+	function __construct($money, $sum){
+		$this->money = $money;
+		$this->sum = $sum;
+	}
+}
+
+class CShowMoney extends CMoney{
+	public function __toString(){
+    return "у вас: ".$this->sum." ".$this->money;
+  }
+}
+
+class CEur extends CShowMoney{
+	public $money;
+	public $sum;
+}
+
+class CRub extends CShowMoney{
+	public $money;
+	public $sum;
+}
+
+$eur = new CEur("eur", 50);
+echo $eur;
+echo "<br>";
+$rub = new CRub("rub", 100);
+echo $rub;
+
+
+?>
